681463f5e546aff6fe09ef5bba0fdcac84b36087
Paola Garay <garay.angela@gmail.com>
2020-10-29 22:54:37 +0100

    Add first analysis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi there!');
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+    encoding: 'utf-8'
+})
+    .split('\n')
+    .map((row: string): string[] => {
+        return row.split(',');
+    })
+
+let manUnitedWings = 0;
+
+for(let match of matches) {
+    if(match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWings++;
+    } else if ( match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWings++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWings} games`)
